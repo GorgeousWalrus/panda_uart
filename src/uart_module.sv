74f86f2baa1afcd9e05cea4f76087f6808892478
Luca Hanel <luca.hanel@hensoldt-cyber.com>
2020-09-03 19:06:43 +0200

    to pc

--- src/uart_module.sv
+++ src/uart_module.sv
@@ -55,28 +55,20 @@ logic [31:0] uart_regs_n[4:0];
 logic [31:0] uart_regs_q[4:0];
 
 // TX signals
-logic [1:0]     tx_cnt;
 logic [7:0]     tx_data;
-logic           tx_incr_cnt;
-logic           tx_rst_cnt;
 logic           tx_done;
 logic           tx_enable;
 
 // RX signals
-logic [1:0]     rx_cnt;
 logic [7:0]     rx_data;
-logic           rx_incr_cnt;
-logic           rx_rst_cnt;
 logic           rx_valid;
 logic           rx_parity_err;
 
 // fifo signals
-logic [31:0]    rx_32data_n, rx_32data_q;
-logic [31:0]    tx_32data_read;
 logic           rx_fifo_read;
 logic           rx_fifo_write_n, rx_fifo_write_q;
+logic [7:0]     rx_data_read;
 logic           rx_empty, rx_full;
-logic [31:0]    tx_32data;
 logic           tx_fifo_read;
 logic           tx_fifo_write_n, tx_fifo_write_q;
 logic           tx_empty, tx_full;
@@ -97,9 +89,6 @@ begin
 
     // UART tx
     tx_enable   = 1'b0;
-    tx_incr_cnt = 1'b0;
-    tx_rst_cnt  = 1'b0;
-    tx_data     = 'b0;
 
     // tx fifo
     tx_fifo_read  = 1'b0;
@@ -107,17 +96,12 @@ begin
     uart_regs_n[STATUS][TX_EMPTY] = tx_empty;
     uart_regs_n[STATUS][TX_FULL] = tx_full;
 
-    // UART rx
-    rx_incr_cnt = 1'b0;
-    rx_rst_cnt  = 1'b0;
-    rx_32data_n = rx_32data_q;
-
     // rx fifo
     rx_fifo_read = 1'b0;
     rx_fifo_write_n = 1'b0;
     uart_regs_n[STATUS][RX_EMPTY] = rx_empty;
     uart_regs_n[STATUS][RX_FULL] = rx_full;
-    uart_regs_n[RX_DATA] = tx_32data_read;
+    uart_regs_n[RX_DATA] = rx_data_read;
 
     // APB slave
     if(apb_bus.PSEL && apb_bus.PENABLE) begin
@@ -149,32 +133,20 @@ begin
     // UART TX
     if(uart_regs_q[CTRL][TX_EN] && (!uart_regs_q[STATUS][TX_EMPTY])) begin
         tx_enable = 1'b1;
-        /* verilator lint_off WIDTH */
-        tx_data = tx_32data >> (8*tx_cnt);
-        /* verilator lint_on WIDTH */
         if(tx_done) begin
             tx_enable = 1'b0;
-            tx_incr_cnt = 1'b1;
-            if(tx_cnt == 2'b11) begin
-                tx_rst_cnt = 1'b1;
-                // notify fifo that we processed the data
-                tx_fifo_read = 1'b1;
-                // If this was the last word in txfifo, disable uart
-                if(uart_regs_q[STATUS][TX_EMPTY])
-                    uart_regs_n[CTRL][TX_EN] = 1'b0;
-            end
+            // notify fifo that we processed the data
+            tx_fifo_read = 1'b1;
+            // If this was the last word in txfifo, disable uart
+            if(uart_regs_q[STATUS][TX_EMPTY])
+                uart_regs_n[CTRL][TX_EN] = 1'b0;
         end
     end
 
     // UART RX
     if((!uart_regs_q[STATUS][RX_FULL]) && uart_regs_q[CTRL][RX_EN]) begin
         if(rx_valid) begin
-            rx_32data_n = rx_32data_q | ({{24{1'b0}}, rx_data} << (8*rx_cnt));
-            rx_incr_cnt = 1'b1;
-            if(rx_cnt == 2'b11) begin
-                rx_rst_cnt = 1'b1;
-                rx_fifo_write_n = 1'b1;
-            end
+            rx_fifo_write_n = 1'b1;
         end
     end
 end
@@ -184,7 +156,7 @@ begin
     if(!rstn) begin
         uart_regs_q[CLK_DIV] <= 32'hffffffff;
         uart_regs_q[CTRL]    <= 'b0;
-        uart_regs_q[STATUS]    <= 'b0;
+        uart_regs_q[STATUS]  <= 'b0;
         uart_regs_q[TX_DATA] <= 'b0;
         uart_regs_q[RX_DATA] <= 'b0;
     end else begin
@@ -194,42 +166,25 @@ begin
         uart_regs_q[TX_DATA] <= uart_regs_n[TX_DATA];
         uart_regs_q[RX_DATA] <= uart_regs_n[RX_DATA];
 
-        // increment and reset TX counter
-        if(tx_rst_cnt)
-            tx_cnt <= 'b0;
-        else if(tx_incr_cnt)
-            tx_cnt <= tx_cnt + 1;
-
-        // increment and reset RX counter
-        if(rx_rst_cnt)
-            rx_cnt <= 'b0;
-        else if(rx_incr_cnt)
-            rx_cnt <= rx_cnt + 1;
-
         // Fifos
         tx_fifo_write_q <= tx_fifo_write_n;
         rx_fifo_write_q <= rx_fifo_write_n;
-        rx_32data_q <= rx_32data_n;
-
-        // reset the read data after successfully writing it into fifo
-        if(rx_fifo_write_q)
-            rx_32data_q <= 'b0;
     end
 end
 
 /* verilator lint_off PINMISSING */
 /* verilator lint_off PINCONNECTEMPTY */
 fifo #(
-    .WIDTH      ( 32        ),
-    .DEPTH      ( 8         ),
+    .WIDTH      ( 8         ),
+    .DEPTH      ( 32        ),
     .ALMOST_EN  ( 0         )
 ) rx_fifo (
     .clk_i      ( clk               ),
     .rstn_i     ( rstn              ),
-    .din_i      ( rx_32data_q       ),
+    .din_i      ( rx_data           ),
     .we_i       ( rx_fifo_write_q   ),
     .re_i       ( rx_fifo_read      ),
-    .dout_o     ( tx_32data_read    ),
+    .dout_o     ( rx_data_read      ),
     .E_o        ( rx_empty          ),
     .F_o        ( rx_full           ),
     .AE_o       (),
@@ -237,18 +192,18 @@ fifo #(
 );
 
 fifo #(
-    .WIDTH      ( 32        ),
-    .DEPTH      ( 8         ),
+    .WIDTH      ( 8         ),
+    .DEPTH      ( 32        ),
     .ALMOST_EN  ( 0         )
 ) tx_fifo (
-    .clk_i      ( clk                   ),
-    .rstn_i     ( rstn                  ),
-    .din_i      ( uart_regs_q[TX_DATA]  ),
-    .we_i       ( tx_fifo_write_q       ),
-    .re_i       ( tx_fifo_read          ),
-    .dout_o     ( tx_32data             ),
-    .E_o        ( tx_empty              ),
-    .F_o        ( tx_full               ),
+    .clk_i      ( clk                     ),
+    .rstn_i     ( rstn                    ),
+    .din_i      ( uart_regs_q[TX_DATA][7:0]),
+    .we_i       ( tx_fifo_write_q         ),
+    .re_i       ( tx_fifo_read            ),
+    .dout_o     ( tx_data                 ),
+    .E_o        ( tx_empty                ),
+    .F_o        ( tx_full                 ),
     .AE_o       (),
     .AF_o       ()
 );
